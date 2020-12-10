f7ae5a26b41227324daf9ab1d4314ec3a4d67c3a
Sander Bakkum <sander@sanderbakkum.nl>
2020-12-10 17:15:56 +0100

    made it shorter

--- day10/day10_2.py
+++ day10/day10_2.py
@@ -1,30 +1,21 @@
 #! /usr/bin/env python
 
+import time
 import sys
 
 if __name__ == '__main__':
-	adapters = [True]
+	a = [0, 0, 1]
 	with open(sys.argv[1]) as f:
 		for num in f:
-			a = int(num.strip())
-			if a >= len(adapters):
-				adapters.extend([False] * (a - len(adapters) + 1))
-			adapters[a] = True
+			x = int(num.strip()) + 2
+			if x >= len(a):
+				a.extend([0] * (x - len(a) + 1))
+			a[x] = 1
 
-	adapters.extend([False, False, True])
-	
-	n_ways = [0] * len(adapters)
+	a.extend([0, 0, 1])
 
-	for i in range(len(n_ways)):
-		if not adapters[i]:
-			continue
-		if i >= 3:
-			n_ways[i] += n_ways[i - 1] + n_ways[i - 2] + n_ways[i - 3]
-		elif i == 2:
-			n_ways[i] += n_ways[i - 1] + n_ways[i - 2]
-		elif i == 1:
-			n_ways[i] += n_ways[i - 1]
-		elif i == 0:
-			n_ways[i] = 1
-
-	print(n_ways[-1])
+	n = [0] * len(a)
+	n[2] = 1
+	for i in range(3, len(n)):
+		n[i] = sum(n[i-3:i]) * a[i]
+	print(n[-1])
